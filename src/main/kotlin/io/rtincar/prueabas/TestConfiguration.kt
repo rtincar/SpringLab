f295ce1452cb1db8fb4e0db63b751993532e290e
rtincar <ul2mac@gmail.com>
2017-11-11 20:06:00 +0100

    Versión inicial del proyecto de pruebas

---
+++ src/main/kotlin/io/rtincar/prueabas/TestConfiguration.kt
@@ -0,0 +1,12 @@
+package io.rtincar.prueabas
+
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class TestConfiguration {
+
+    /*@Bean
+    fun reactiveWebServerFactory() = NettyReactiveWebServerFactory(9090)*/
+}
--- src/test/groovy/io/rtincar/pruebas/AccountSpec.groovy
+++ src/test/groovy/io/rtincar/pruebas/AccountSpec.groovy
@@ -2,11 +2,14 @@ package io.rtincar.pruebas
 
 import groovy.json.JsonOutput
 import io.rtincar.kanbanboard.configuration.AccountConfiguration
+import io.rtincar.prueabas.TestConfiguration
+import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.http.MediaType
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.function.BodyInserters
 import spock.lang.Narrative
 import spock.lang.Specification
@@ -23,22 +26,23 @@ I want to create an account
 ''')
 //@ContextConfiguration(classes = [AccountConfiguration])
 @SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        classes = [AccountConfiguration]/*,
-        properties = [ "spring.main.webApplicationType=reactive" ]*/
+        webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+        classes = [TestConfiguration, AccountConfiguration],
+        properties = [ "spring.main.webApplicationType=reactive" ]
 )
 class AccountSpec extends Specification {
 
     @LocalServerPort
     Integer port
 
+
+
+    void "Should return ok"() {
+        given: "JSON account data"
     WebTestClient client = WebTestClient
             .bindToServer()
             .baseUrl("http://localhost:${port}")
             .build()
-
-    void "Should return ok"() {
-        given: "JSON account data"
 
         def jsonAccount = JsonOutput.toJson([a: 2, b: 'Data'])
 
